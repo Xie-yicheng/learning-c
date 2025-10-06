1547c144065149bc22d65b312e949931958be64a
Xie-yicheng <3204853491@qq.com>
2025-10-06 10:35:33 +0800

    Add files via upload

---
+++ detest-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int a = 5;
+    float b = 10.5;
+    float sum = a + b;
+    printf("The sum is: %.2f\n", sum);
+    return 0;
+}
